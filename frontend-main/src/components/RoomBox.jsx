9ee0edf5dbcbff9dfa8570269e2a64eb07994d08
eunbie07 <eunbie07@gmail.com>
2025-08-14 00:46:14 +0900

    fix: import 경로에 .js 확장자 추가 및 Docker 빌드 디버그 추가 - useKeyboardControls 훅 파일 확인됨

--- frontend-main/src/components/RoomBox.jsx
+++ frontend-main/src/components/RoomBox.jsx
@@ -46,13 +46,13 @@ import { DraggableFurnitureWithCollision } from "./3D/DraggableFurniture";
 import { DraggableHuman } from "./3D/DraggableHuman";
 
 // 분리된 훅들
-import { useRoomState } from "../hooks/useRoomState";
-import { useToast } from "../hooks/useToast";
-import { useFurnitureHandlers } from "../hooks/useFurnitureHandlers";
-import { useKeyboardControls } from "../hooks/useKeyboardControls";
-import { useWallFloorSettings } from "../hooks/useWallFloorSettings";
-import { usePanelStates } from "../hooks/usePanelStates";
-import { useFurnitureConversion } from "../hooks/useFurnitureConversion";
+import { useRoomState } from "../hooks/useRoomState.js";
+import { useToast } from "../hooks/useToast.js";
+import { useFurnitureHandlers } from "../hooks/useFurnitureHandlers.js";
+import { useKeyboardControls } from "../hooks/useKeyboardControls.js";
+import { useWallFloorSettings } from "../hooks/useWallFloorSettings.js";
+import { usePanelStates } from "../hooks/usePanelStates.js";
+import { useFurnitureConversion } from "../hooks/useFurnitureConversion.js";
 
 // 분리된 상수들
 import {
