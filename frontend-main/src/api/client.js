44fabbe6615b8a57fc8363e506a360a7010d60a4
eunbie07 <eunbie07@gmail.com>
2025-08-14 17:48:23 +0900

    fastapi_url_change

--- frontend-main/src/api/client.js
+++ frontend-main/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 개발/운영 환경 모두 원격 서버 주소를 사용하도록 통일합니다.
-const FASTAPI_BASE_URL = 'http://13.55.21.100:3001';
+const FASTAPI_BASE_URL = 'https://dev-bidanee.site';
 
 const client = axios.create({
   baseURL: FASTAPI_BASE_URL,
