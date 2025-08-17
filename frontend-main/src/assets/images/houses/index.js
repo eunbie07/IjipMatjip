5a0d1d9918a68da6734a366edaae072c063d0563
bidanee <dleksql119@gmail.com>
2025-08-17 17:25:31 +0000

    edit: kakaoMap 수정

---
+++ frontend-main/src/assets/images/houses/index.js
@@ -0,0 +1,17 @@
+// src/assets/images/houses/index.js
+
+import homePinIcon from './home_pin.png';
+import schoolIcon from './school.png';
+import subwayIcon from './subway.png';
+import hospitalIcon from './hospital.png';
+import martIcon from './mart.png';
+import parkIcon from './park.png';
+
+export const ICONS = {
+  home_pin: homePinIcon,
+  school: schoolIcon,
+  subway: subwayIcon,
+  hospital: hospitalIcon,
+  mart: martIcon,
+  park: parkIcon,
+};
